75e7abed9df40358a9fae48129089e139388d619
KROSSEYE <30990184+KROSSEYE@users.noreply.github.com>
2023-09-13 13:36:13 +0100

    add ace editor for yaml

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import yaml
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,29 @@ with open(config_file_path, 'r', encoding='utf8') as yaml_file:
 @app.route('/')
 def index():
     return render_template('index.html', websites_data=websites_data)
+
+
+@app.route('/edit')
+def edit_yaml_page():
+    return render_template('edit_yaml.html')
+
+
+@app.route('/get-yaml')
+def get_yaml():
+    with open(config_file_path, 'r', encoding='utf8') as yaml_file:
+        yaml_content = yaml_file.read()
+    return yaml_content
+
+
+@app.route('/save-yaml', methods=['POST'])
+def save_yaml():
+    yaml_data = request.form.get('yaml')
+    try:
+        with open(config_file_path, 'w', encoding='utf8') as yaml_file:
+            yaml_file.write(yaml_data)
+        return "OK", 200
+    except Exception as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
